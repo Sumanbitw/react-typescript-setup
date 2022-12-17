2a692ab6f1e299285b28230d17987ab25c849d71
Sumanbitw <007sumanghosh@gmail.com>
2022-12-17 18:40:34 +0530

    setup with webpack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import "./style.css";
 
 export const App = () => {
-  return <h1>React Typescript setup</h1>;
+  return (
+    <h1>
+      React Typescript setup {process.env.NODE_ENV} - {process.env.name}
+    </h1>
+  );
 };
