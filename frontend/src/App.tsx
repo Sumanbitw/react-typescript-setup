42c3878818aff5291cbb1df2b5b6a7c922ace541
Sumanbitw <007sumanghosh@gmail.com>
2022-12-17 18:54:58 +0530

    setup eslint & prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./style.css";
+import React from 'react'
+import './style.css'
 
 export const App = () => {
   return (
     <h1>
       React Typescript setup {process.env.NODE_ENV} - {process.env.name}
     </h1>
-  );
-};
+  )
+}
